ae6f5025f98b408c12a8a28ddc57cff65812bcbb
nlp <yuanhang.nju@gmail.com>
2013-02-02 03:04:59 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gusblr::Application.config.secret_token = '7635a33d6fae06fdf769b02dd857f7abd93ad487ca340e4e23715620887e6358c19862e888a6583c26a547342b3fe6f38d630d9b59e206937f1cccf1e5bd57b0'
